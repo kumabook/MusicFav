bba6d7e3b568407a7379f1fada2d27f3113ab6f1
Hiroki Kumamoto <kumabook@live.jp>
2015-04-11 01:05:34 +0900

    Change blue color

--- MusicFav/UIColorExtenstion.swift
+++ MusicFav/UIColorExtenstion.swift
@@ -16,7 +16,7 @@ extension UIColor {
     class var themeLight:  UIColor { return build(r: 240/255, g: 139/255, b:  51/255, a: 0.8) }
     class var green:       UIColor { return build(r:  71/255, g: 234/255, b: 126/255, a: 1.0) }
     class var red:         UIColor { return build(r: 219/255, g:  36/255, b:  91/255, a: 1.0) }
-    class var blue:        UIColor { return build(r:  75/255, g:       0, b: 130/255, a: 1.0) }
+    class var blue:        UIColor { return build(r: 100/255, g: 149/255, b: 237/255, a: 1.0) }
     class var transparent: UIColor { return build(r:       0, g:       0, b:       0, a:   0) }
     class var lightGray:   UIColor { return build(r: 214/255, g: 214/255, b: 214/255, a: 1.0) }
 }
