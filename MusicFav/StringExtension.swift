fc5c955303baeaff4de28067f16c18980dac5688
Hiroki Kumamoto <kumabook@live.jp>
2015-08-16 23:56:27 +0900

    Remove utility because they are moved to kumabook/Breit

--- MusicFav/StringExtension.swift
+++ MusicFav/StringExtension.swift
@@ -9,27 +9,11 @@
 import Foundation
 
 extension String {
-    func localize() -> String {
-        return NSLocalizedString(self, tableName: "Localizable",
-                                          bundle: NSBundle.mainBundle(),
-                                           value: self,
-                                         comment: self)
-    }
-
     static func tutorialString(key: String) -> String {
         return NSLocalizedString(key, tableName: "Tutorial",
                                           bundle: NSBundle.mainBundle(),
                                            value: key,
                                          comment: key)
 
-    }
-
-    func toURL() -> NSURL? {
-        if let url = NSURL(string: self) {
-            return url
-        } else if let str = stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet()) {
-            return NSURL(string: str)
-        }
-        return nil
     }
 }
