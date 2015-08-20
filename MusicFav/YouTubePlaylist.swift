a26ad8a39f3005970da63dd8a1a1f3fba488beb1
Hiroki Kumamoto <kumabook@live.jp>
2015-08-21 01:11:49 +0900

    Implement YouTubePlaylist model classes and its loader

---
+++ MusicFav/YouTubePlaylist.swift
@@ -0,0 +1,102 @@
+//
+//  YouTubePlaylist.swift
+//  MusicFav
+//
+//  Created by Hiroki Kumamoto on 8/18/15.
+//  Copyright (c) 2015 Hiroki Kumamoto. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import MusicFeeder
+
+public class YouTubePlaylist: YouTubeResource, Hashable, Equatable {
+    public class var url: String { return "https://www.googleapis.com/youtube/v3/playlists" }
+    public class var params: [String:String] { return ["mine": "true"] }
+    public var hashValue: Int { return id.hashValue }
+    public let etag:        String
+    public let id:          String
+    public let kind:        String
+    public let title:       String!
+    public let description: String!
+    public let publishedAt: String?
+    public let thumbnails:  [String:String]
+    public let resourceId:  [String:String]
+
+    public required init(json: JSON) {
+        let snippet = json["snippet"].dictionaryValue
+        etag        = json["etag"].stringValue
+        id          = json["id"].stringValue
+        kind        = json["kind"].stringValue
+        title       = snippet["title"]!.stringValue
+        description = snippet["description"]!.stringValue
+        publishedAt = snippet["publishedAt"]?.string
+        thumbnails  = ChannelResource.thumbnails(snippet)
+        resourceId  = ChannelResource.resourceId(snippet)
+    }
+    public var thumbnailURL: NSURL? {
+        if let url = thumbnails["default"] { return NSURL(string: url) }
+        else if let url = thumbnails["medium"]  { return NSURL(string: url) }
+        else if let url = thumbnails["high"]    { return NSURL(string: url) }
+        else                                    { return nil }
+    }
+}
+
+public func ==(lhs: YouTubePlaylist, rhs: YouTubePlaylist) -> Bool {
+    return lhs.id == rhs.id
+}
+
+public class YouTubePlaylistItem: YouTubeResource {
+    public class var url: String { return "https://www.googleapis.com/youtube/v3/playlistItems" }
+    public class var params: [String:String] { return [:] }
+    public let etag:        String
+    public let id:          String
+    public let kind:        String
+    public let title:       String!
+    public let description: String!
+    public let publishedAt: String?
+    public let thumbnails: [String:String]
+    public let resourceId: [String:String]
+
+    public let position:      UInt
+    public let videoId:       String
+    public let startAt:       String?
+    public let endAt:         String?
+    public let note:          String?
+    public let privacyStatus: String?
+
+    public var track:         Track
+
+    public required init(json: JSON) {
+        let snippet        = json["snippet"].dictionaryValue
+        let contentDetails = json["contentDetails"].dictionaryValue
+        let status         = json["status"].dictionaryValue
+        etag               = json["etag"].stringValue
+        id                 = json["id"].stringValue
+        kind               = json["kind"].stringValue
+        title              = snippet["title"]!.stringValue
+        description        = snippet["description"]!.stringValue
+        publishedAt        = snippet["publishedAt"]?.string
+        thumbnails         = ChannelResource.thumbnails(snippet)
+        resourceId         = ChannelResource.resourceId(snippet)
+
+        position           = snippet["position"]!.uIntValue
+        videoId            = contentDetails["videoId"]!.stringValue
+        startAt            = contentDetails["startAt"]?.stringValue
+        endAt              = contentDetails["endAt"]?.stringValue
+        note               = contentDetails["note"]?.stringValue
+        privacyStatus      = status["privacyStatus"]?.stringValue
+
+        track              = Track(provider: Provider.Youtube,
+                                        url: "https://www.youtube.com/watch?v=\(videoId)",
+                                 identifier: videoId,
+                                      title: title)
+    }
+
+    public var thumbnailURL: NSURL? {
+        if let url = thumbnails["default"] { return NSURL(string: url) }
+        else if let url = thumbnails["medium"] { return NSURL(string: url) }
+        else if let url = thumbnails["high"]   { return NSURL(string: url) }
+        else                                   { return nil }
+    }
+}
---
+++ MusicFav/YouTubePlaylistLoader.swift
@@ -0,0 +1,118 @@
+//
+//  YouTubePlaylistLoader.swift
+//  MusicFav
+//
+//  Created by Hiroki Kumamoto on 8/18/15.
+//  Copyright (c) 2015 Hiroki Kumamoto. All rights reserved.
+//
+
+import Foundation
+import ReactiveCocoa
+import Result
+import Box
+
+class YouTubePlaylistLoader {
+    enum State {
+        case Init
+        case Fetching
+        case Normal
+        case Error
+    }
+
+    enum Event {
+        case StartLoading
+        case CompleteLoading
+        case FailToLoad
+    }
+
+    var playlists:       [YouTubePlaylist]
+    var itemsOfPlaylist: [YouTubePlaylist: [YouTubePlaylistItem]]
+
+    var state:          State
+    var signal:         Signal<Event, NSError>
+    var sink:           SinkOf<ReactiveCocoa.Event<Event, NSError>>
+
+    var playlistsPageToken:  String?
+    var playlistsDisposable: Disposable?
+
+    var itemsPageTokenOfPlaylist:  [YouTubePlaylist: String]
+    var itemsDisposableOfPlaylist: [YouTubePlaylist: Disposable?]
+
+    init() {
+        playlists          = []
+        itemsOfPlaylist    = [:]
+        self.state         = .Init
+        let pipe           = Signal<Event, NSError>.pipe()
+        signal             = pipe.0
+        sink               = pipe.1
+        playlistsPageToken = ""
+        itemsPageTokenOfPlaylist  = [:]
+        itemsDisposableOfPlaylist = [:]
+    }
+
+    func clear() {
+        playlists          = []
+        itemsOfPlaylist    = [:]
+        playlistsPageToken = ""
+        state              = .Normal
+        playlistsDisposable?.dispose()
+        itemsPageTokenOfPlaylist  = [:]
+        itemsDisposableOfPlaylist = [:]
+    }
+
+    func needFetchPlaylists() -> Bool {
+        return playlistsPageToken != nil
+    }
+
+    func fetchPlaylists() {
+        if !needFetchPlaylists() { return }
+        switch state {
+        case .Init:     playlistsDisposable = fetchNextPlaylists().start()
+        case .Fetching: break
+        case .Normal:   playlistsDisposable = fetchNextPlaylists().start()
+        case .Error:    playlistsDisposable = fetchNextPlaylists().start()
+        }
+    }
+
+    func needFetchPlaylistItems(playlist: YouTubePlaylist) -> Bool {
+        return itemsPageTokenOfPlaylist[playlist] != nil
+    }
+
+    func fetchPlaylistItems(playlist: YouTubePlaylist) {
+        if !needFetchPlaylistItems(playlist) { return }
+        switch state {
+        case .Init:     playlistsDisposable = fetchNextPlaylistItems(playlist).start()
+        case .Fetching: break
+        case .Normal:   playlistsDisposable = fetchNextPlaylistItems(playlist).start()
+        case .Error:    playlistsDisposable = fetchNextPlaylistItems(playlist).start()
+        }
+    }
+
+    private func fetchNextPlaylists() -> SignalProducer<Void, NSError> {
+        state = State.Fetching
+        sink.put(ReactiveCocoa.Event<Event, NSError>.Next(Box(.StartLoading)))
+        return YouTubeAPIClient.sharedInstance.fetchPlaylists(playlistsPageToken) |> map {
+            self.playlists.extend($0.items)
+            self.playlistsPageToken = $0.nextPageToken
+            for i in $0.items {
+                self.itemsOfPlaylist[i]          = []
+                self.itemsPageTokenOfPlaylist[i] = ""
+            }
+            self.sink.put(ReactiveCocoa.Event<Event, NSError>.Next(Box(.CompleteLoading)))
+            self.state = State.Normal
+        }
+    }
+
+    private func fetchNextPlaylistItems(playlist: YouTubePlaylist) -> SignalProducer<Void, NSError> {
+        state = State.Fetching
+        sink.put(ReactiveCocoa.Event<Event, NSError>.Next(Box(.StartLoading)))
+        let pageToken = itemsPageTokenOfPlaylist[playlist]!
+        return YouTubeAPIClient.sharedInstance.fetchPlaylistItems(playlist, pageToken: pageToken) |> map {
+            self.itemsOfPlaylist[playlist]?.extend($0.items)
+            self.itemsPageTokenOfPlaylist[playlist] = $0.nextPageToken
+            println(            self.itemsPageTokenOfPlaylist[playlist])
+            self.sink.put(ReactiveCocoa.Event<Event, NSError>.Next(Box(.CompleteLoading)))
+            self.state = State.Normal
+        }
+    }
+}
