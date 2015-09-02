0ad9fe10812f71cf41153f835ce21893567d3396
Hiroki Kumamoto <kumabook@live.jp>
2015-09-02 20:54:14 +0900

    Ajust StreamTreeViewCell

--- MusicFav/StreamTreeViewCell.swift
+++ MusicFav/StreamTreeViewCell.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class StreamTreeViewCell: UITableViewCell {
-    let perWidth: Int = 24
-    var indent:   Int = 0
+    let thumbWidth: CGFloat = 32
+    let margin:     CGFloat = 12
+    let perWidth:   Int = 24
+    var indent:     Int = 0
+
     override func layoutSubviews() {
         super.layoutSubviews()
+        self.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        if let f = self.imageView?.frame {
+            self.imageView?.frame = CGRect(x: f.origin.x , y: f.origin.y, width: thumbWidth, height: f.height)
+        }
         self.imageView?.move(x: CGFloat(indent * perWidth), y: 0)
-        self.textLabel?.move(x: CGFloat(indent * perWidth), y: 0)
+        let imageRect = self.imageView!.frame
+        let labelRect = self.textLabel!
+        self.textLabel?.frame = CGRect(x: imageRect.origin.x + imageRect.width + margin,
+                                       y: imageRect.origin.y,
+                                   width: labelRect.frame.width,
+                                  height: labelRect.frame.height)
     }
 }
