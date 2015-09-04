bc2a3751aafecc299c5922ae292c5e88ec9fe071
Hiroki Kumamoto <kumabook@live.jp>
2015-09-05 00:11:47 +0900

    Show category select view if user is not logged in

--- MusicFav/AddStreamTableViewController.swift
+++ MusicFav/AddStreamTableViewController.swift
@@ -107,28 +107,8 @@ class AddStreamTableViewController: UITableViewController {
         if let indexPaths = tableView.indexPathsForSelectedRows() {
             let subscribables: [Stream] = getSubscribables()
             Logger.sendUIActionEvent(self, action: "add", label: "")
-            if isLoggedIn {
-                let ctc = CategoryTableViewController(subscribables: subscribables, streamListLoader: streamListLoader)
-                navigationController?.pushViewController(ctc, animated: true)
-            } else {
-                MBProgressHUD.showHUDAddedTo(self.navigationController!.view, animated: true)
-                subscribables.reduce(SignalProducer<[Subscription], NSError>(value: [])) {
-                    combineLatest($0, self.streamListLoader.subscribeTo($1, categories: [])) |> map {
-                        var list = $0.0; list.append($0.1); return list
-                    }
-                    } |> start(
-                        next: { subscriptions in
-                            MBProgressHUD.hideHUDForView(self.navigationController!.view, animated:false)
-                        }, error: { e in
-                            MBProgressHUD.hideHUDForView(self.navigationController!.view, animated:false)
-                            let ac = CloudAPIClient.alertController(error: e, handler: { (action) in })
-                        }, completed: {
-                            MBProgressHUD.showCompletedHUDForView(self.navigationController!.view, animated: true, duration: 1.0, after: {
-                                self.streamListLoader.refresh()
-                                self.navigationController?.dismissViewControllerAnimated(true, completion: {})
-                            })
-                    })
-            }
+            let ctc = CategoryTableViewController(subscribables: subscribables, streamListLoader: streamListLoader)
+            navigationController?.pushViewController(ctc, animated: true)
         }
     }
 }
