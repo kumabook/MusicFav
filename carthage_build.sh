1e7fc5ea3ae0f38860bfb220eb352d469e4fb6e3
Hiroki Kumamoto <kumabook@live.jp>
2017-12-26 22:20:59 +0900

    carthge_buid.sh creates two phase cache: checkout and build

--- carthage_build.sh
+++ carthage_build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-if ! diff Cartfile.resolved Carthage/Cartfile.resolved &>/dev/null; then
+if ! diff Cartfile.resolved Carthage/Cartfile.checkout.resolved &>/dev/null; then
   echo '--- Carthage checkout ---'
   carthage checkout --no-use-binaries
+  echo 'copy Cartfile.resolved to Carthage/Cartfile.checkout.resolved'
+  cp Cartfile.resolved Carthage/Cartfile.checkout.resolved
+  echo '--- carthage checkout success ---'
+fi
+
+if ! diff Cartfile.resolved Carthage/Cartfile.resolved &>/dev/null; then
   echo '--- carthage build ---'
   carthage build --platform iOS
   echo 'copy Cartfile.resolved'
-  cp Cartfile.resolved Carthage
+  cp Cartfile.resolved Carthage/Cartfile.resolved
   echo '--- carthage install success ---'
 fi
