c775619bbecd228e10281f5c6db3f539eef33ad5
Hiroki Kumamoto <kumabook@live.jp>
2017-12-28 23:24:54 +0900

    Add --cache-builds option

--- carthage_build.sh
+++ carthage_build.sh
@@ -2,7 +2,7 @@
 
 if ! diff Cartfile.resolved Carthage/Cartfile.resolved &>/dev/null; then
   echo '--- carthage build ---'
-  carthage build --platform iOS
+  carthage build --cache-builds --platform iOS
   echo 'copy Cartfile.resolved'
   cp Cartfile.resolved Carthage/Cartfile.resolved
   echo '--- carthage install success ---'
