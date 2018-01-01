77f20d18249a06721153e9adc18f023aa9d9a184
Hiroki Kumamoto <kumabook@live.jp>
2018-01-01 15:39:56 +0900

    Use OAuthSwift for SoundCloud login

--- MusicFav/SoundCloudAPIClient.swift
+++ MusicFav/SoundCloudAPIClient.swift
@@ -12,32 +12,18 @@ import SwiftyJSON
 import NXOAuth2Client
 import ReactiveSwift
 import Alamofire
+import OAuthSwift
+import Prephirences
 
-open class OAuth2ClientDelegate: NSObject, NXOAuth2ClientDelegate {
-    public typealias RequestCallback = (DataResponse<Any>) -> Void
-    open var pendingRequests: [(APIClient.Router, RequestCallback)]
-    public override init() {
-        pendingRequests = []
-    }
-    func addPendingRequest(_ request: APIClient.Router, callback: @escaping RequestCallback) {
-        pendingRequests.append((request, callback))
-    }
-    func restartPendingRequests() {
-        for req in pendingRequests {
-            APIClient.sharedInstance.manager.request(req.0).responseJSON(options: .allowFragments, completionHandler: req.1)
+open class SoundCloudOAuthRequestRetrier: OAuthRequestRetrier {
+    public override func refreshed(_ succeeded: Bool) {
+        if succeeded {
+            APIClient.credential = oauth.client.credential
+            APIClient.accessToken = oauth.client.credential.oauthToken
+        } else {
+            APIClient.credential = nil
+            APIClient.accessToken = nil
         }
-        pendingRequests = []
-    }
-    open func oauthClientNeedsAuthentication(_ client: NXOAuth2Client!) {}
-    open func oauthClientDidGetAccessToken(_ client: NXOAuth2Client!) {}
-    open func oauthClient(_ client: NXOAuth2Client!, didFailToGetAccessTokenWithError error: Error!) {
-        restartPendingRequests()
-    }
-    open func oauthClientDidLoseAccessToken(_ client: NXOAuth2Client!) {}
-    open func oauthClientDidRefreshAccessToken(_ client: NXOAuth2Client!) {
-        APIClient.refreshAccount(NXOAuth2Account(accountWith: client.accessToken,
-                                                 accountType: APIClient.accountType))
-        restartPendingRequests()
     }
 }
 
@@ -47,50 +33,21 @@ extension APIClient {
     public static var authUrl        = "https://soundcloud.com/connect"
     public static var tokenUrl       = "\(baseUrl)/oauth2/token"
     public static var scope          = Set([] as [String])
-    public static var redirectUrl    = "http://localhost/"
-    public static var accountType    = "SoundCloud"
+    public static var redirectUri    = "http://localhost/"
     public static var keyChainGroup  = "SoundCloud"
 
     static let errorResponseKey = "com.alamofire.serialization.response.error.response"
 
     fileprivate static let userDefaults = UserDefaults.standard
-    static func newManager() -> SessionManager {
-        let m = Alamofire.SessionManager()
-        if let token = APIClient.accessToken {
-            m.session.configuration.httpAdditionalHeaders = ["Authorization": "Bearer \(token)"]
-        }
-        return m
-    }
+    static var oauth: OAuth2Swift!
 
-    static var oauth2clientDelegate: OAuth2ClientDelegate!
-    static func createOauth2client(_ delegate: OAuth2ClientDelegate) -> NXOAuth2Client? {
-        if let accessToken = account?.accessToken {
-            return NXOAuth2Client(clientID: clientId,
-                              clientSecret: clientSecret,
-                              authorizeURL: URL(string: authUrl)!,
-                                  tokenURL: URL(string: tokenUrl)!,
-                               accessToken: accessToken,
-                             keyChainGroup: keyChainGroup,
-                                persistent: true,
-                                  delegate: delegate)
-        } else {
-            return nil
+    static var credential: OAuthSwiftCredential? {
+        get {
+            return KeychainPreferences.sharedInstance[APIClient.keyChainGroup] as? OAuthSwiftCredential
         }
-    }
-
-    fileprivate static var _account: NXOAuth2Account?
-    public static var account: NXOAuth2Account? {
-        if let a = _account {
-            return a
+        set {
+            KeychainPreferences.sharedInstance[APIClient.keyChainGroup] = newValue
         }
-        let store = NXOAuth2AccountStore.sharedStore() as! NXOAuth2AccountStore
-        for account in store.accounts as! [NXOAuth2Account] {
-            if account.accountType == APIClient.keyChainGroup {
-                _account = account
-                return account
-            }
-        }
-        return nil
     }
 
     fileprivate static var _me: User?
@@ -113,31 +70,53 @@ extension APIClient {
     }
 
     public static func clearAllAccount() {
-        let store = NXOAuth2AccountStore.sharedStore() as! NXOAuth2AccountStore
-        for account in store.accounts as! [NXOAuth2Account] {
-            if account.accountType == APIClient.keyChainGroup {
-                store.removeAccount(account)
-            }
-        }
-        _account = nil
+        credential = nil
         SoundCloudKit.APIClient.accessToken = nil
     }
 
-    static func refreshAccount(_ account: NXOAuth2Account) {
-        clearAllAccount()
-        let store = NXOAuth2AccountStore.sharedStore() as! NXOAuth2AccountStore
-        store.addAccount(account)
-        SoundCloudKit.APIClient.accessToken = account.accessToken.accessToken
+    public class func setup() {
+        loadConfig()
+        oauth = OAuth2Swift(
+            consumerKey:    clientId,
+            consumerSecret: clientSecret,
+            authorizeUrl:   authUrl,
+            accessTokenUrl: tokenUrl,
+            responseType:   "code"
+        )
+        if let c = credential {
+            oauth.client.credential.oauthToken          = c.oauthToken
+            oauth.client.credential.oauthTokenSecret    = c.oauthTokenSecret
+            oauth.client.credential.oauthTokenExpiresAt = c.oauthTokenExpiresAt
+            oauth.client.credential.oauthRefreshToken   = c.oauthRefreshToken
+            APIClient.sharedInstance.manager.session.configuration.httpAdditionalHeaders = ["Authorization": "Bearer \(c.oauthToken)"]
+        }
+        APIClient.sharedInstance.manager.retrier = SoundCloudOAuthRequestRetrier(oauth)
+        APIClient.accessToken = credential?.oauthToken
     }
 
-    public class func setup() {
-        APIClient.oauth2clientDelegate = OAuth2ClientDelegate()
-        loadConfig()
-        if let _ = SoundCloudKit.APIClient.me {
-            SoundCloudKit.APIClient.accessToken = account?.accessToken.accessToken
-        } else {
-            SoundCloudKit.APIClient.clearAllAccount()
-        }
+    static func authorize(_ viewController: UIViewController, callback: (() -> ())? = nil) {
+        let vc = OAuthViewController()
+        viewController.addChildViewController(vc)
+        oauth.authorizeURLHandler = vc
+        let _ = oauth.authorize(
+            withCallbackURL: URL(string: APIClient.redirectUri)!,
+            scope: APIClient.scope.joined(separator: ","),
+            state: "SoundCloud",
+            success: { credential, response, parameters in
+                APIClient.credential = credential
+                APIClient.accessToken = credential.oauthToken
+                APIClient.sharedInstance.fetchMe().on(
+                    failed: { error in
+                        if let callback = callback { callback() }
+                }, value: { user in
+                    APIClient.me = user
+                    AppDelegate.shared.reload()
+                    if let callback = callback { callback() }
+                }).start()
+        },
+            failure: { error in
+                if let callback = callback { callback() }
+        })
     }
 
     public class func loadConfig() {
@@ -168,22 +147,9 @@ extension APIClient {
     }
 
 
-    func fetch(_ route: Router, callback: @escaping OAuth2ClientDelegate.RequestCallback) {
-        self.manager.request(route).validate(statusCode: 200..<300).responseJSON(options: .allowFragments) {(response: DataResponse<Any>) -> Void in
-            if response.result.isFailure {
-                if let oauth2client = APIClient.createOauth2client(APIClient.oauth2clientDelegate) {
-                    APIClient.oauth2clientDelegate.addPendingRequest(route, callback: callback)
-                    oauth2client.refreshAccessToken()
-                    return
-                }
-            }
-            callback(response)
-        }
-    }
-
     func fetchItem<T: JSONInitializable>(_ route: Router) -> SignalProducer<T, NSError> {
         return SignalProducer { observer, disposable in
-            let callback: OAuth2ClientDelegate.RequestCallback = {(response: DataResponse<Any>) -> Void in
+            let req = self.manager.request(route).validate(statusCode: 200..<300).responseJSON(options: .allowFragments) {(response: DataResponse<Any>) -> Void in
                 if let e = response.result.error as NSError? {
                     if let r = response.response {
                         observer.send(error: NSError(domain: e.domain, code: e.code, userInfo: [APIClient.errorResponseKey:r]))
@@ -195,14 +161,15 @@ extension APIClient {
                     observer.sendCompleted()
                 }
             }
-            self.fetch(route, callback: callback)
+            disposable.observeEnded {
+                req.cancel()
+            }
         }
     }
 
     func fetchItems<T: JSONInitializable>(_ route: Router) -> SignalProducer<[T], NSError> {
         return SignalProducer { observer, disposable in
-            let callback: OAuth2ClientDelegate.RequestCallback = {(response: DataResponse<Any
-                >) -> Void in
+            let req = self.manager.request(route).validate(statusCode: 200..<300).responseJSON(options: .allowFragments) {(response: DataResponse<Any>) -> Void in
                 if let e = response.result.error as NSError? {
                     if let r = response.response {
                         observer.send(error: NSError(domain: e.domain, code: e.code, userInfo: [APIClient.errorResponseKey:r]))
@@ -214,7 +181,9 @@ extension APIClient {
                     observer.sendCompleted()
                 }
             }
-            self.fetch(route, callback: callback)
+            disposable.observeEnded {
+                req.cancel()
+            }
         }
     }
 
