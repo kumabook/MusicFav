8714e491af3d9cb70d08f39cb453e730330d238d
Hiroki Kumamoto <kumabook@live.jp>
2018-01-03 03:56:23 +0900

    Disable cache of Carthage on travis

--- carthage_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! diff Cartfile.resolved Carthage/Cartfile.resolved &>/dev/null; then
-  echo '--- carthage build ---'
-  carthage build --cache-builds --platform iOS
-  echo 'copy Cartfile.resolved'
-  cp Cartfile.resolved Carthage/Cartfile.resolved
-  echo '--- carthage install success ---'
-fi
--- carthage_checkout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-if ! diff Cartfile.resolved Carthage/Cartfile.checkout.resolved &>/dev/null; then
-  echo '--- Carthage checkout ---'
-  carthage checkout
-  echo 'copy Cartfile.resolved to Carthage/Cartfile.checkout.resolved'
-  cp Cartfile.resolved Carthage/Cartfile.checkout.resolved
-  echo '--- carthage checkout success ---'
-fi
