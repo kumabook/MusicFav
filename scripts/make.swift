75bd0d0442b041b953ad717a91e37340019f61a4
Hiroki Kumamoto <kumabook@live.jp>
2018-01-21 15:18:34 +0900

    Bump up version to 0.5.2(78)

--- scripts/make.swift
+++ scripts/make.swift
@@ -15,7 +15,7 @@ let cp         = "/bin/cp"
 let xcodebuild = "/usr/bin/xcodebuild"
 let carthage   = "/usr/local/bin/carthage"
 let agvtool    = "/usr/bin/agvtool"
-let version    = "0.5.1"
+let version    = "0.5.2"
 
 enum Task: String {
     case Config  = "config"
