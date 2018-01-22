15f30360025fa35edcaf87709bb1cc574395b7a1
Hiroki Kumamoto <kumabook@live.jp>
2018-01-22 19:06:29 +0900

    Implement Track extension: Add constructor with SPTTrack

---
+++ MusicFav/Track.swift
@@ -0,0 +1,42 @@
+//
+//  Track.swift
+//  MusicFav
+//
+//  Created by Hiroki Kumamoto on 2018/01/22.
+//  Copyright © 2018 Hiroki Kumamoto. All rights reserved.
+//
+
+import Foundation
+import Spotify
+import MusicFeeder
+
+extension Track {
+    convenience init(spotifyTrack spt: SPTPartialTrack) {
+        self.init(id:           "",
+                  provider:     .spotify,
+                  url:          spt.uri.absoluteString,
+                  identifier:   spt.identifier,
+                  title:        spt.name,
+                  duration:     spt.duration,
+                  thumbnailUrl: spt.album?.smallestCover?.imageURL,
+                  artworkUrl:   spt.album?.largestCover?.imageURL,
+                  audioUrl:     spt.previewURL,
+                  ownerId:      spt.artists.get(0).flatMap { $0 as? SPTPartialArtist }.flatMap { $0.identifier },
+                  ownerName:    spt.artists.get(0).flatMap { $0 as? SPTPartialArtist }.flatMap { $0.name },
+                  status:       .available)
+    }
+    convenience init(spotifyTrack spt: SPTPartialTrack, spotifyAlbum album: SPTAlbum) {
+        self.init(id:           "",
+                  provider:     .spotify,
+                  url:          spt.uri.absoluteString,
+                  identifier:   spt.identifier,
+                  title:        spt.name,
+                  duration:     spt.duration,
+                  thumbnailUrl: album.smallestCover?.imageURL,
+                  artworkUrl:   album.largestCover?.imageURL,
+                  audioUrl:     spt.previewURL,
+                  ownerId:      spt.artists.get(0).flatMap { $0 as? SPTPartialArtist }.flatMap { $0.identifier },
+                  ownerName:    spt.artists.get(0).flatMap { $0 as? SPTPartialArtist }.flatMap { $0.name },
+                  status:       .available)
+    }
+}
