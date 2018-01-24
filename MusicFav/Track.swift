1bbc403dfc7efdcbeffdc5a326838a4616694949
Hiroki Kumamoto <kumabook@live.jp>
2018-01-25 01:42:40 +0900

    Add Track.init(item: MPMediaItem)

--- MusicFav/Track.swift
+++ MusicFav/Track.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import MediaPlayer
 import Spotify
 import MusicFeeder
 
@@ -38,5 +39,34 @@ extension Track {
                   ownerId:      spt.artists.get(0).flatMap { $0 as? SPTPartialArtist }.flatMap { $0.identifier },
                   ownerName:    spt.artists.get(0).flatMap { $0 as? SPTPartialArtist }.flatMap { $0.name },
                   status:       .available)
+    }
+    @available(iOS 10.3, *)
+    convenience init(item: MPMediaItem) {
+        let country = Track.appleMusicCurrentCountry ?? "US"
+        var url = ""
+        if item.playbackStoreID != "0" {
+            url = "https://geo.itunes.apple.com/\(country)/xxxx/xxxx/\(item.playbackStoreID)"
+        }
+        self.init(id:           "\(item.persistentID)",
+                  provider:     .appleMusic,
+                  url:          url,
+                  identifier:   item.playbackStoreID,
+                  title:        item.title,
+                  duration:     item.playbackDuration,
+                  thumbnailUrl: nil,
+                  artworkUrl:   nil,
+                  audioUrl:     item.assetURL,
+                  ownerId:      nil,
+                  ownerName:    item.artist,
+                  status:       Track.Status.available,
+                  expiresAt:    Int64.max,
+                  publishedAt:  Int64.max,
+                  createdAt:    Date().timestamp,
+                  updatedAt:    Date().timestamp,
+                  state:        EnclosureState.alive,
+                  isLiked:      nil,
+                  isSaved:      nil,
+                  isPlayed:     nil)
+        mediaItem = item
     }
 }
